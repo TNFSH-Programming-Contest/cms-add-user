7ad7e473652e31b96b99a8ccf47ae48e0d33ce74
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-09-29 11:40:10 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+import csv
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('users', help='filename of users list, contains 4 columns: first_name (class), last_name (real name), username, plaintext password')
+parser.add_argument('-c', '--contest', dest='contests', action='append')
+parser.add_argument('--dry-run', action='store_true', help="Don't run any commands, but display them")
+parser.add_argument('--no-user', action='store_true', help="Don't run cmsAddUser")
+parser.add_argument('--no-participation', action='store_true', help="Don't run cmsAddParticipation")
+parser.set_defaults(
+    contests=[],
+)
+args = parser.parse_args()
+print(args)
+
+
+users = []
+with open(args.users, 'r', encoding='utf8') as f:
+    reader = csv.reader(f, )
+    for row in reader:
+        users.append(row)
+
+for u in users:
+    if not args.no_user:
+        cmd = ['cmsAddUser', u[0], u[1], u[2], '-p', u[3]]
+        print(' '.join(cmd))
+        if not args.dry_run:
+            subprocess.run(cmd)
+
+    if not args.no_participation:
+        for c in args.contests:
+            cmd = ['cmsAddParticipation', u[2], '-c', c]
+            print(' '.join(cmd))
+            if not args.dry_run:
+                subprocess.run(cmd)
